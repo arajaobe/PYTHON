561d18e20e4f9e229e1ca97db32755b82efec454
arajaobe <andylraj@gmail.com>
2026-05-26 23:06:56 +0300

    update mod 4

---
+++ MODULE_04/ft_vault_security.py
@@ -0,0 +1,46 @@
+
+def secure_archive(filename: str, action: str="r", content: str="" ) -> tuple[bool, str]:
+	if action == "r":
+		try:
+			with open(filename, "r") as f:
+				content_file = f.read()
+			print("Using 'secure_archive' to read from a regular file:")
+			return(True, content_file)
+		except FileNotFoundError as e:
+			print("Using 'secure_archive' to read from a nonexistent file:")
+			return(False, f"{e}")
+		except PermissionError as e:
+			print("Using 'secure_archive' to read from an inaccessible file:")
+			return(False, f"{e}")
+	elif action == "w":
+		try:
+			with open(filename, "w") as f:
+				f.write(content)
+			print("Using 'secure_archive' to write previous content to a new file:")
+			return (True, "Content successfully written to file")
+		except Exception as e:
+			print("Using 'secure_archive' to write previous content to a new file:")
+			return(False, f"{e}")
+	else:
+		return None
+
+def main():
+	print("=== Cyber Archives Security ===\n")
+
+	result = secure_archive("not/existing/file")
+	print(result)
+	print("\n")
+	result = secure_archive("ask.passwd")
+	print(result)
+	print("\n")
+	result = secure_archive("ancient_fragment.txt")
+	print(result)
+	print("\n")
+	result = secure_archive("result.txt", "w", "bobo")
+	print(result)
+
+
+if __name__ == "__main__":
+	main()
+
+
--- MODULE_04/test.py
+++ MODULE_04/test.py
@@ -23,7 +23,55 @@ def save_file(filename: str, content: str) -> None:
 
 #new_content_str = "".join(new_content)
 
-print(new_content)
+#print(new_content)
+
+def secure_archive(filename: str, action: str="r", content: str="" ) -> tuple[bool, str]:
+	if action == "r":
+		try:
+			with open(filename, "r") as f:
+				content_file = f.read()
+			res = True
+		except FileNotFoundError as e:
+			error = e
+			error_message = "Using 'secure_archive' to read from a nonexistent file:"
+			res = False
+		except PermissionError as e:
+			error = e
+			error_message = "Using 'secure_archive' to read from an inaccessible file:"
+			res = False
+		if res:
+			print("Using 'secure_archive' to read from a regular file:")
+			return (res, content_file)
+		else:
+			print(error_message)
+			return(res, error)
+	elif action == "w":
+		try:
+			with open(filename, "w") as f:
+				f.write(content)
+			res = True
+		except Exception as e:
+			error = e
+			res = False
+		if res:
+			return (res, "'Content successfully written to file'")
+		else:
+			return (res, error)
+
+#print(secure_archive("ancient_fragmnt.txt", "r"))
+
+
+
+
+print(secure_archive("ancient_ragment.txt", "w"))
+
+
+
+
+
+
+
+
 
 f.close()
 
