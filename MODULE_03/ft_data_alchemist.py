8b815d202a741e31f5d79a2ff944f4d8c1f14665
arajaobe <andylraj@gmail.com>
2026-07-26 12:31:30 +0300

    3

--- MODULE_03/ft_data_alchemist.py
+++ MODULE_03/ft_data_alchemist.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import random
 
-def is_capitalize(value: str)->int:
-	if value[0].isupper():
-		return 1
-	return 0
+def is_capitalize(value: str)->bool:
+	return value[0].isupper()
 
 def capitalizer(value: list[str])->list[str]:
 	res = [arg.capitalize() for arg in value]
@@ -14,7 +12,7 @@ def only_capitalize(value: list[str])->list[str]:
 	res = [arg for arg in value if is_capitalize(arg)]
 	return res
 
-def main():
+def main() -> None:
 	print("=== Game Data Alchemist ===\n")
 
 	players = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
@@ -25,15 +23,14 @@ def main():
 
 	print("New list with all names capitalized:", list_key)
 	print("New list of capitalized names only:", list_only_capitalize)
-	print("\n")
+	print("")
 
 	score_dict = {name : random.randint(1, 1000) for name in list_key}
-	list_score = list(score_dict.values())
-	average = round(sum(list_score) / len(list_key), 2)
+	average = round(sum(score_dict.values()) / len(score_dict), 2)
 	high_score_dict = {key : value for key, value in score_dict.items() if value > average }
 
 	print("Score dict:", score_dict)
-	print("Score average:", average)
+	print(f"Score average is {average}")
 	print("High scores:", high_score_dict)
 
 if __name__ == "__main__":
