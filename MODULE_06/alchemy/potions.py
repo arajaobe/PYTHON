b9f1f85baaf1f5b19856515bb08dc98348061892
arajaobe <andylraj@gmail.com>
2026-08-03 12:14:08 +0300

    creat mod 6

---
+++ MODULE_06/alchemy/potions.py
@@ -0,0 +1,13 @@
+
+from alchemy.elements import create_air, create_earth
+from elements import create_water, create_fire
+
+def healing_potion() -> str:
+    return(f"Healing potion brewed with '{create_earth()}'"
+           f" and '{create_air()}'")
+
+
+def strength_potion() -> str:
+    return(f"Strength potion brewed with '{create_fire()}'"
+           f" and '{create_water()}'")
+
---
+++ MODULE_06/ft_alembic_1.py
@@ -0,0 +1,6 @@
+
+from elements import create_water
+
+print(create_water())
+
+
